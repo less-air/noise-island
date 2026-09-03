b9b8132028dcd9c138f888fe004a57ce2c298168
less-air <nirojassa@proton.me>
2026-09-03 18:52:31 +0100

    url embed

--- app.js
+++ app.js
@@ -458,7 +458,7 @@ fetch("episodes.json")
   });
 
 function soundCloudEmbedUrl(url) {
-  return "https://w.soundcloud.com/player/?" +
+  return "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/soundcloud%253Atracks%253A2325516866&color=%23f4a6d2&inverse=true&auto_play=false&show_user=false" +
     new URLSearchParams({
       url: url,
       inverse: "true",
